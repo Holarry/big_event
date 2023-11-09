3dd21b2c382b4846d44e9873d8b12c86ea0f165f
Holary <2908183713@qq.com>
2023-11-09 17:26:51 +0800

    feat: 实现用户登录功能

--- src/main/java/com/holary/controller/UserController.java
+++ src/main/java/com/holary/controller/UserController.java
@@ -3,12 +3,17 @@ package com.holary.controller;
 import com.holary.pojo.Result;
 import com.holary.pojo.User;
 import com.holary.service.UserService;
+import com.holary.utils.JwtUtil;
+import com.holary.utils.Md5Util;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Author: Holary
@@ -31,7 +36,7 @@ public class UserController {
      */
     @PostMapping("/register")
     public Result register(@Pattern(regexp = "^\\S{5,16}$") String username, @Pattern(regexp = "^\\S{5,16}$") String password) {
-        // 检查询用户是否存在
+        // 查询用户名是否已经存在
         User user = userService.findByUsername(username);
         if (user != null) {
             return Result.error(username + "已存在!");
@@ -39,5 +44,34 @@ public class UserController {
         // 注册
         userService.register(username, password);
         return Result.success();
+    }
+
+    /**
+     * description: 用户登录
+     *
+     * @param username: 用户名
+     * @param password: 密码
+     * @return: com.holary.pojo.Result<java.lang.String>
+     */
+    @PostMapping("/login")
+    public Result login(@Pattern(regexp = "^\\S{5,16}$") String username, @Pattern(regexp = "^\\S{5,16}$") String password) {
+        // 查询用户是否存在
+        User user = userService.findByUsername(username);
+        if (user == null) {
+            return Result.error(username + "不存在!");
+        }
+
+        // 判断用户名和密码是否匹配
+        if (!user.getPassword().equals(Md5Util.getMD5String(password))) {
+            return Result.error("用户名或密码错误!");
+        }
+
+        // 生成JWT令牌
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", user.getId());
+        claims.put("username", user.getUsername());
+        String token = JwtUtil.genToken(claims);
+
+        return Result.success(token);
     }
 }
